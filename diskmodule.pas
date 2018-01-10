5ac2e463cf22538c5ddc7bb7ccf091cb6716b7bb
tedsmith28@hotmail.com <tedsmith28@hotmail.com>
2018-01-10 14:44:27 +0000

    Updated CHANGELOG.md and fixed remaining reference to udisk

--- diskmodule.pas
+++ diskmodule.pas
@@ -1025,7 +1025,7 @@ begin
   // Get all technical specifications about a user selected disk and save it
   DiskInfoProcessUDISKS := TProcess.Create(nil);
   DiskInfoProcessUDISKS.Options := [poWaitOnExit, poUsePipes];
-  DiskInfoProcessUDISKS.CommandLine := 'udisks --show-info ' + Treeview1.Selected.Text;
+  DiskInfoProcessUDISKS.CommandLine := 'udisksctl info ' + Treeview1.Selected.Text;
   DiskInfoProcessUDISKS.Execute;
 
   diskinfoUDISKS := TStringList.Create;
