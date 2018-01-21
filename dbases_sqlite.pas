ad2e3f43a8ff5c0e98de9005b0d365176804190d
Ted Smith <1560598+tedsmith@users.noreply.github.com>
2018-01-21 23:44:44 +0000

    set /tmp as path for DB on OSX and Linux

--- dbases_sqlite.pas
+++ dbases_sqlite.pas
@@ -154,8 +154,8 @@ begin
       SQLDBLibraryLoaderOSX.LoadLibrary;
       // Set the filename of the sqlite database
       strFileNameRandomiser := FormatDateTime('YYYY-MM-DD_HH-MM-SS', Now); // use a randomised filename suffix to enable multiple instances
-      // Determine a safe place to write the SQLite database file to;
-      SafePlaceForDB := GetAppConfigDir(false); //GetUserDir returns /Users/Username/ on OSX but GetAppConfigDir safer I think;
+      //  write the SQLite database file to /tmp;
+      SafePlaceForDB := '/tmp/';
       if ForceDirectories(SafePlaceForDB) then
       begin
         SQLite3Connection1.DatabaseName := SafePlaceForDB + 'QuickHashDBOSX_' + strFileNameRandomiser + '.sqlite';
@@ -218,19 +218,18 @@ begin
       SQLDBLibraryLoaderLinux.LoadLibrary;
       // Set the filename of the sqlite database
       strFileNameRandomiser := FormatDateTime('YYYY-MM-DD_HH-MM-SS', Now); // use a randomised filename suffix to enable multiple instances
-      SQLite3Connection1.DatabaseName := 'QuickHashDBLinux_' + strFileNameRandomiser + '.sqlite';
-      // Create the database and connect to it
-      CreateDatabase(SQLite3Connection1.DatabaseName);
-
-      if SQLIte3Connection1.Connected then
+      //  write the SQLite database file to /tmp
+      SafePlaceForDB := '/tmp/';
+      if ForceDirectories(SafePlaceForDB) then
       begin
-        // None of this is visibile to the user. We just need to alert him if connection fails
-        lblConnectionStatus.Caption:= 'SQLite3 Database connection active';
+        SQLite3Connection1.DatabaseName := SafePlaceForDB + 'QuickHashDBLinux_' + strFileNameRandomiser + '.sqlite';
+        // Create the database
+        CreateDatabase(SQLite3Connection1.DatabaseName);
+        if SQLIte3Connection1.Connected then lblConnectionStatus.Caption:= 'SQLite3 Database connection active';
       end
-        else
+      else
         begin
-          ShowMessage('Cannot create SQLite database. Probably SQLite is not installed on your system (could not find libsqlite3.so.0). Exiting');
-          abort;
+          Showmessage('Could not create folder ' + SafePlaceForDB + ' for ' + SQLite3Connection1.DatabaseName);
         end;
       end;
     {$endif}
