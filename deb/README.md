9c34ae46331392a137edd695f0acaab1c2aba6c1
darealshinji <djcj@gmx.de>
2023-10-21 03:38:17 +0200

    apply patches to be able to link with -pie

--- deb/README.md
+++ deb/README.md
@@ -1,7 +1,7 @@
 How To
 ======
 
-Install the required build-dependencies: `sudo apt install --no-install-recommends lazarus build-essential fakeroot lintian debhelper`
+Install the required build-dependencies: `sudo apt install --no-install-recommends lazarus build-essential fakeroot lintian debhelper quilt`
 
 Copy the directory `debian` and the script `buildpackage.sh` into QuickHash's source directory and run `./buildpackage.sh`.
 Remember to keep the changelog up to date.
--- deb/buildpackage.sh
+++ deb/buildpackage.sh
@@ -3,6 +3,7 @@
 LANG=C
 LANGUAGE=C
 LC_ALL=C
+QUILT_PATCHES="debian/patches"
 
 packages="$(grep '^Package: ' debian/control | cut -d ' ' -f2)"
 source=$(dpkg-parsechangelog | grep '^Source:' | cut -d ' ' -f2)
@@ -10,11 +11,29 @@ version=$(dpkg-parsechangelog | grep '^Version:' | cut -d ' ' -f2)
 arch=$(dpkg-architecture -qDEB_HOST_ARCH)
 log="../build_${source}_${version}_${arch}.log"
 
+# check build dependencies
 dpkg-checkbuilddeps || exit 1
 
-echo "Note: log filtering will replace '$PWD' with '<<BUILDDIR>>'" | tee $log
+rm -f $log
+touch $log
+
+# apply patches
+if [ ! -f .patches_applied ]; then
+  quilt push -a | tee -a $log
+  touch .patches_applied
+fi
+
+# build package
+echo "Note: log filtering will replace '$PWD' with '<<BUILDDIR>>'" | tee -a $log
 dpkg-buildpackage -j`nproc` -rfakeroot -b -us -uc 2>&1 | sed "s|$PWD|<<BUILDDIR>>|g" | tee -a $log
 echo "" | tee -a $log
+
+# revert patches
+if [ -f .patches_applied ]; then
+  quilt pop -a && rm .patches_applied
+fi
+
+# show infos about package
 
 for p in $packages ; do
   f="../${p}_${version}_${arch}.deb"
